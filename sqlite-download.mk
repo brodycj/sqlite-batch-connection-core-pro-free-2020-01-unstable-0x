4cdd01811d08f30cbbc7b5780109d8d6cbc25afb
Christopher J. Brody <chris.brody+brodybits@gmail.com>
2020-06-29 11:33:55 -0400

    separate SQLITE_PARENT in sqlite-download.mk

--- sqlite-download.mk
+++ sqlite-download.mk
@@ -2,5 +2,7 @@ SQLITE_AMALGAMATION := sqlite-amalgamation-3320300
 
 SQLITE_ZIP := $(SQLITE_AMALGAMATION).zip
 
-# TBD part of this URL is expected to change each year:
-SQLITE_ZIP_URL := https://sqlite.org/2020/$(SQLITE_ZIP)
+# Note that this part of this URL is expected to change each year:
+SQLITE_PARENT := 2020
+
+SQLITE_ZIP_URL := https://sqlite.org/$(SQLITE_PARENT)/$(SQLITE_ZIP)
